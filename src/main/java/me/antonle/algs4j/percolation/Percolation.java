cd3d26ad5863547cd39f50a8c4d9bfa180228121
Anton Le <le@physics.msu.ru>
2016-02-28 21:35:48 +0300

    Percolation backwash fixed another way

--- src/main/java/me/antonle/algs4j/percolation/Percolation.java
+++ src/main/java/me/antonle/algs4j/percolation/Percolation.java
@@ -7,7 +7,9 @@ public class Percolation {
     private final int N;
     private final boolean[][] grid;
     private final WeightedQuickUnionUF uf;
+    private final WeightedQuickUnionUF ufTop;
     private final int virtualTopSiteIdx;
+    private final int virtualBottomSiteIdx;
     private boolean hasOpenedSites = false;
 
     /**
@@ -19,11 +21,15 @@ public class Percolation {
         }
         this.N = N;
         this.grid = new boolean[N][N];
-        this.uf = new WeightedQuickUnionUF(N * N + 1);
+        this.uf = new WeightedQuickUnionUF(N * N + 2);
+        this.ufTop = new WeightedQuickUnionUF(N * N + 1);
         this.virtualTopSiteIdx = N * N;
+        this.virtualBottomSiteIdx = N * N + 1;
         // link top row to their virtual site
         for (int i = 1; i <= N; i++) {
             uf.union(virtualTopSiteIdx, toUFIndex(1, i));
+            uf.union(virtualBottomSiteIdx, toUFIndex(N, i));
+            ufTop.union(virtualTopSiteIdx, toUFIndex(1, i));
         }
     }
 
@@ -37,15 +43,19 @@ public class Percolation {
             grid[i - 1][j - 1] = true;
             // check the bounds to find the neighbours
             if (i != 1 && isOpen(i - 1, j)) {
+                ufTop.union(toUFIndex(i, j), toUFIndex(i - 1, j));
                 uf.union(toUFIndex(i, j), toUFIndex(i - 1, j));
             }
             if (i != N && isOpen(i + 1, j)) {
+                ufTop.union(toUFIndex(i, j), toUFIndex(i + 1, j));
                 uf.union(toUFIndex(i, j), toUFIndex(i + 1, j));
             }
             if (j != 1 && isOpen(i, j - 1)) {
+                ufTop.union(toUFIndex(i, j), toUFIndex(i, j - 1));
                 uf.union(toUFIndex(i, j), toUFIndex(i, j - 1));
             }
             if (j != N && isOpen(i, j + 1)) {
+                ufTop.union(toUFIndex(i, j), toUFIndex(i, j + 1));
                 uf.union(toUFIndex(i, j), toUFIndex(i, j + 1));
             }
         }
@@ -65,21 +75,13 @@ public class Percolation {
      */
     public boolean isFull(int i, int j) {
         checkBounds(i, j);
-        return isOpen(i, j) && uf.connected(virtualTopSiteIdx, toUFIndex(i, j));
+        return isOpen(i, j) && ufTop.connected(virtualTopSiteIdx, toUFIndex(i, j));
     }
 
     public boolean percolates() {
-        return hasOpenedSites && hasConnectionWithLastRow();
+        return hasOpenedSites && uf.connected(virtualTopSiteIdx, virtualBottomSiteIdx);
     }
 
-    private boolean hasConnectionWithLastRow() {
-        for (int i = 1; i <= N; i++) {
-            if (uf.connected(virtualTopSiteIdx, toUFIndex(N, i))) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     private int toUFIndex(final int row, final int column) {
         return (row - 1) * N + (column - 1);
